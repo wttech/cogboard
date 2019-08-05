9bf3c22819b82cbc14c0d18492c4e77f00f80f63
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2019-08-05 15:27:33 +0200

    CR fixes

--- cogboard-webapp/src/components/widgets/types/SonarQubeWidget.js
+++ cogboard-webapp/src/components/widgets/types/SonarQubeWidget.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from '@emotion/styled/macro';
 
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 const StyledButton = styled(Button)`
   width: 100%
 `;
 
 const SonarQubeWidgetContent = props => {
-  const { metrics = {}, id = '-', url = '#', version = '-', date = '' } = props;
+  const {metrics = {}, id = '-', url = '#', version = '-', date = ''} = props;
 
   return (
     <>
       <Typography
-        variant="caption">
+        variant="caption"
+      >
         <p>Version: {version}</p>
         <p>Timestamp: {date}</p>
         {Object.entries(metrics).map(([metric, val]) =>
@@ -23,7 +24,8 @@ const SonarQubeWidgetContent = props => {
       </Typography>
       <StyledButton
         variant="outlined"
-        href={url}>
+        href={url}
+      >
         #{id}
       </StyledButton>
     </>
