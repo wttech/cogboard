651a8f0d80882979215488dcd221f052628a6b7a
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2019-08-05 15:27:33 +0200

    CR fixes

--- cogboard-webapp/src/components/widgets/dialogFields/SonarQubeMetricsInput.js
+++ cogboard-webapp/src/components/widgets/dialogFields/SonarQubeMetricsInput.js
@@ -1,44 +1,45 @@
 import React from 'react';
 import styled from '@emotion/styled/macro';
 
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import ListItemText from '@material-ui/core/ListItemText';
-import Select from '@material-ui/core/Select';
-import Checkbox from '@material-ui/core/Checkbox';
+import { Input } from '@material-ui/core';
+import { InputLabel } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
+import { ListItemText } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { Checkbox } from '@material-ui/core';
 
-  const StyledFormControl = styled(FormControl)`
+const StyledFormControl = styled(FormControl)`
       margin: theme.spacing(1),
       minWidth: 120,
       maxWidth: 300,
   `;
 
-  const allMetrics = [
-    "blocker_violations",
-    "critical_violations",
-    "major_violations",
-    "minor_violations",
-    "info_violations",
-    "bugs",
-    "code_smells",
-    "vulnerabilities"
-  ];
+const allMetrics = [
+  "blocker_violations",
+  "critical_violations",
+  "major_violations",
+  "minor_violations",
+  "info_violations",
+  "bugs",
+  "code_smells",
+  "vulnerabilities"
+];
 
 const SonarQubeMetricsInput = props => {
-  const { onChange } = props;
-  let { value } = props;
-  value = value || ['blocker_violations','critical_violations','major_violations','minor_violations'];
+  const {onChange} = props;
+  const inputId = 'sonarqube-metrics-input';
+  let {value} = props;
+  value = value || ['blocker_violations', 'critical_violations', 'major_violations', 'minor_violations'];
 
   return (
     <StyledFormControl>
-      <InputLabel htmlFor="select-multiple-checkbox">Metrics</InputLabel>
+      <InputLabel htmlFor={inputId}>Metrics</InputLabel>
       <Select
         multiple
         value={value}
         onChange={onChange}
-        input={<Input id="select-multiple-checkbox"/>}
+        input={<Input id={inputId}/>}
         renderValue={value => `${value.length} selected`}
       >
         {allMetrics.map(name => (
--- cogboard-webapp/src/components/widgets/types/JenkinsJobWidget.js
+++ cogboard-webapp/src/components/widgets/types/JenkinsJobWidget.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled/macro';
 
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 const StyledButton = styled(Button)`
   width: 100%
