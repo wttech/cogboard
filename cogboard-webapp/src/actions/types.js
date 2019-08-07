da9a1364f521679621ebae78a549d2eabefc7194
marek.golebiowski <marek.golebiowski@PL-MAC-C02X52MPJHD2.local>
2019-08-07 12:55:15 +0200

    Add dataChanged flag to store and show/hide save button functionality

--- cogboard-webapp/src/actions/types.js
+++ cogboard-webapp/src/actions/types.js
@@ -5,3 +5,5 @@ export const UPDATE_WIDGET = 'UPDATE_WIDGET';
 export const EDIT_WIDGET = 'EDIT_WIDGET';
 export const ADD_WIDGET = 'ADD_WIDGET';
 export const DELETE_WIDGET = 'DELETE_WIDGET';
+export const CHANGE_DATA = 'CHANGE_DATA';
+export const SAVE_DATA_SUCCESS = 'SAVE_DATA_SUCCESS';
