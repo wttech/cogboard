96547ced85ed81049acae7b217fb2602ed04b07e
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2019-08-19 16:42:50 +0200

    CLAN6-69 - open links in new tab

--- cogboard-webapp/src/components/styled/index.js
+++ cogboard-webapp/src/components/styled/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 
 import { Button } from '@material-ui/core';
 
-export const FullWidthButtonOutlined = (props) => <Button {...props} fullWidth="true" variant="outlined"/>;
+export const FullWidthButtonOutlined = (props) => <Button {...props} fullWidth="true" variant="outlined" target="_blank"/>;
