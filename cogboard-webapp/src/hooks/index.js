a3c7b431778d87919542a1fc99151ca8b816a04b
Michal Skrzypek <michal.skrzypek@cognifide.com>
2019-08-30 20:36:50 +0200

    Move useInterval hook to hooks directory

--- cogboard-webapp/src/hooks/index.js
+++ cogboard-webapp/src/hooks/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 export const useDialogToggle = () => {
   const [dialogOpened, setDialogOpened] = useState(false);
@@ -25,3 +25,23 @@ export const useFormData = (data) => {
 
   return { values, handleChange };
 };
+
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
