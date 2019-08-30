b000ffa96fc3047489d1e24ef02738dd4dee4872
Michal Skrzypek <michal.skrzypek@cognifide.com>
2019-08-30 20:29:55 +0200

    Code refactoring

--- cogboard-webapp/src/constants/index.js
+++ cogboard-webapp/src/constants/index.js
@@ -1,7 +1,7 @@
 export const URL = {
-    LOAD_DATA: '/api/config', // for front dev change to '/data.json'
-    SAVE_DATA: '/api/config/save',
-    UPDATE_WIDGET: '/api/widget/update'
+  LOAD_DATA: '/api/config', // for front dev change to '/data.json'
+  SAVE_DATA: '/api/config/save',
+  UPDATE_WIDGET: '/api/widget/update'
 };
 export const COLUMNS_MIN = 1;
 export const COLUMNS_MAX = 20;
