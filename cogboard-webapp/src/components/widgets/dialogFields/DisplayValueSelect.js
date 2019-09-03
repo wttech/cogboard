13d9f45d58b11b3138604adcecd26d6c64399a20
Michal Skrzypek <michal.skrzypek@cognifide.com>
2019-09-03 14:31:28 +0200

    Make select field more generic

---
+++ cogboard-webapp/src/components/widgets/dialogFields/DisplayValueSelect.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {FormControl, MenuItem} from '@material-ui/core';
+import DropdownField from '../../DropdownField';
+import styled from "@emotion/styled/macro";
+
+/**
+ * Displays dropdown field using passed array of objects with the following properties: display, value.
+ * Example: [
+ *  {
+ *      display: "name1"
+ *      value: "value1"
+ *  },
+ *  {
+ *      display: "name2"
+ *      value: "value2"
+ *  }
+ * ]
+ */
+const DisplayValueSelect = props => {
+    const {dropdownItems} = props;
+
+    const StyledDropdown = styled(FormControl)`
+      display: flex;
+      margin-bottom: 12px;
+      min-width: 300px;
+    `;
+
+    return (
+        <StyledDropdown>
+            <DropdownField dropdownItems={dropdownItems} {...props}>
+                {dateFormats => dateFormats.map(dateFormat =>
+                    <MenuItem key={dateFormat.value} value={dateFormat.value}>{dateFormat.display}</MenuItem>
+                )}
+            </DropdownField>
+        </StyledDropdown>
+    );
+};
+
+export default DisplayValueSelect;
