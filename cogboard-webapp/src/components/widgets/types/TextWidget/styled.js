41585a5812b6d02cd08ec9da26af1e6fd461f4b7
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2019-09-06 10:16:10 +0200

    Text widget style fix

--- cogboard-webapp/src/components/widgets/types/TextWidget/styled.js
+++ cogboard-webapp/src/components/widgets/types/TextWidget/styled.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled/macro';
 import { Typography } from '@material-ui/core';
 
 export const VerticalText = styled(Typography)`
+  margin-top: auto;
   transform: rotate(-90deg);
 `;
 
