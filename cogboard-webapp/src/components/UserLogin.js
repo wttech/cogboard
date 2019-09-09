e2c5b18f0bb6374e4af6ab0e42d28760772aa50e
Michal Skrzypek <michal.skrzypek@cognifide.com>
2019-09-09 14:16:41 +0200

    Imports organized

--- cogboard-webapp/src/components/UserLogin.js
+++ cogboard-webapp/src/components/UserLogin.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {Button, IconButton, TextField, Typography} from '@material-ui/core';
+import {AccountCircle, PowerSettingsNew} from '@material-ui/icons';
 
 import {useDialogToggle, useFormData, useSnackbarToggle} from '../hooks';
 import {login, logout} from '../actions/thunks';
-
-import {Button, IconButton, TextField, Typography} from '@material-ui/core';
-import {AccountCircle, PowerSettingsNew} from '@material-ui/icons';
 import {StyledFieldset} from './styled';
 import AppDialog from './AppDialog';
 import SnackbarWithVariant from "./SnackbarWithVariant";
