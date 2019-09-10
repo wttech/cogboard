3c9663293d3c5bc78909984eca99233e7718f3b2
Michal Skrzypek <michal.skrzypek@cognifide.com>
2019-09-10 11:17:13 +0200

    Fix styling and proptypes

--- cogboard-webapp/src/components/ConfirmationDialog.js
+++ cogboard-webapp/src/components/ConfirmationDialog.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import {bool, func, string} from 'prop-types';
+import {func, string} from 'prop-types';
 import Button from "@material-ui/core/Button";
 import {Typography} from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import styled from "@emotion/styled/macro";
 import AppDialog from "./AppDialog";
+
+const StyledCancelButton = styled(Button)`
+  margin-left: 1em;
+`;
+
+const StyledButtonBox = styled(Box)`
+  margin-top: 1em;
+`;
 
 const ConfirmationDialog = props => {
   const {open, title, content, handleOk, labelOk, handleCancel, labelCancel} = props;
@@ -15,44 +24,30 @@ const ConfirmationDialog = props => {
       open={open}
       title={title}
     >
-      <Grid
-        container
-        justify="flex-end"
-        spacing={1}
-      >
-        <Grid item xs={12}>
-          <Typography id="confirmation-dialog-content">
-            {content}
-          </Typography>
-        </Grid>
-        <Grid item xs={handleCancel ? 8 : 10}>
-        </Grid>
-        <Grid item xs={2}>
-          <Button onClick={handleOk} variant="contained" color="primary" autoFocus>
-            {labelOk}
-          </Button>
-        </Grid>
-        {handleCancel && <Grid item xs={2}>
-          <Button onClick={handleCancel} variant="outlined" color="primary">
-            {labelCancel}
-          </Button>
-        </Grid>}
-      </Grid>
+      <Typography id="confirmation-dialog-content">
+        {content}
+      </Typography>
+      <StyledButtonBox display="flex" justifyContent="flex-end">
+        <Button onClick={handleOk} variant="contained" color="primary" autoFocus>
+          {labelOk}
+        </Button>
+        {handleCancel &&
+        <StyledCancelButton onClick={handleCancel} variant="outlined" color="primary">
+          {labelCancel}
+        </StyledCancelButton>}
+      </StyledButtonBox>
     </AppDialog>
   );
 };
 
 ConfirmationDialog.propTypes = {
-  open: bool.isRequired,
   handleOk: func.isRequired,
-  title: string,
   content: string,
   labelOk: string,
   labelCancel: string
 };
 
 ConfirmationDialog.defaultProps = {
-  title: '',
   content: '',
   labelOk: 'OK',
   labelCancel: 'Cancel'
