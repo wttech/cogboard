d9564d6e2f5b6c430bc70420fea7ee1b6329b963
marcin_wit_cog <marcin.witaszek@cognifide.com>
2019-09-12 13:53:49 +0200

    Fixed checkbox widget edit button z-index issue;added last update time with changed placement

--- cogboard-webapp/src/components/LastUpdate.js
+++ cogboard-webapp/src/components/LastUpdate.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Typography} from "@material-ui/core";
+import styled from '@emotion/styled/macro';
+import { Typography } from "@material-ui/core";
+
+const StyledTypography = styled(Typography)`
+  display: flex;
+  flex: 1;
+  align-items: flex-end;
+`;
 
 const LastUpdate = ({ lastUpdateTime }) => {
-    return (
-        <Typography align='left' color='textSecondary' variant='caption' gutterBottom={false}>
-            Last update: {lastUpdateTime}
-        </Typography>
-    );
+  return (
+    <StyledTypography align='left' color='textSecondary' variant='caption' gutterBottom={false}>
+      Last update: {lastUpdateTime}
+    </StyledTypography>
+  );
 };
 
 export default LastUpdate;
