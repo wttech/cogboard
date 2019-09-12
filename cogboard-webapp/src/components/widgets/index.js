a375ec69a9514d6fc7247ec561fa2314bf0f335e
Michal Skrzypek <michal.skrzypek@cognifide.com>
2019-09-12 18:08:44 +0200

    Add time display size and new date formats

--- cogboard-webapp/src/components/widgets/index.js
+++ cogboard-webapp/src/components/widgets/index.js
@@ -52,7 +52,7 @@ const widgetTypes = {
   WorldClockWidget: {
     name: 'World Clock widget',
     component: WorldClockWidget,
-    dialogFields: ['TimeZoneId', 'DateTimeFormat']
+    dialogFields: ['TimeZoneId', 'DateTimeFormat', 'TextSize']
   },
   IFrameEmbedWidget: {
     name: 'IFrame Embed widget',
