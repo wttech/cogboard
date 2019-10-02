81725bac37424aedb20b7b83c768fd323452bb8d
yahor.mikheyenka <yahor.mikheyenka@cognifide.com>
2019-10-02 15:30:34 +0200

    feature/widget-service-check-improvements - remove unused constants

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/CogboardConstants.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/CogboardConstants.kt
@@ -25,8 +25,6 @@ class CogboardConstants {
         const val PROP_EVENT_ADDRESS = "event.address"
         const val PROP_ENDPOINT = "endpoint"
         const val PROP_ARRAY = "array"
-        const val PROP_TEXT = "text"
-        const val PROP_TEXT_SIZE = "textSize"
 
         const val EVENT_SEND_MESSAGE_TO_WEBSOCKET = "cogboard.websocket.message"
         const val EVENT_SAVE_BOARDS_CONFIG = "cogboard.config.boards.save"
