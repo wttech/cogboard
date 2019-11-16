5ec5e6718a40a2af827725a113839f210ac6617b
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2019-11-16 03:10:18 +0100

    saveBoardsConfig test added

--- cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/service/BoardsAndWidgetsServiceTest.kt
+++ cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/service/BoardsAndWidgetsServiceTest.kt
@@ -4,28 +4,40 @@ import com.cognifide.cogboard.config.ConfigType
 import com.cognifide.cogboard.config.validation.boards.BoardsValidator
 import com.cognifide.cogboard.storage.ContentRepository
 import com.cognifide.cogboard.storage.VolumeStorage
+import io.restassured.internal.assertion.Assertion
 import io.vertx.core.json.JsonObject
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import java.io.File
 
 internal class BoardsAndWidgetsServiceTest {
+    @BeforeAll
+    fun init() {
+        val boardPath =  BoardsAndWidgetsServiceTest::class.java.getResource("/board").path
+        File("$boardPath/widgets").delete()
+        File("$boardPath/widgets").mkdir()
+    }
 
     @Test
     fun saveBoardsConfig() {
         //given
-        val conf = File("D:/Projects/cogboard/cogboard/mnt/config.json").readText()
+        val boardPath =  BoardsAndWidgetsServiceTest::class.java.getResource("/board").path
+        val conf = BoardsAndWidgetsServiceTest::class.java.getResource("/board/ui-board-config.json")
+                .readText()
 
         val storage = VolumeStorage(ConfigType.BOARDS,
-                "D:/Projects/cogboard/cogboard/mnt/config.json", BoardsValidator)
-        val contentRepository = ContentRepository("D:/Projects/cogboard/cogboard/mnt")
+                "$boardPath/server-config.json", BoardsValidator)
+        val contentRepository = ContentRepository("$boardPath/widgets")
         val underTest = BoardsAndWidgetsService(JsonObject(), storage, contentRepository)
 
         //when
         underTest.saveBoardsConfig(JsonObject(conf))
 
         //then
-
+        assertTrue(File("$boardPath/widgets/widget1.json").exists())
     }
 }
