affb849349b786c3e62068e210766c5cf2d57075
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2019-11-20 09:48:47 +0100

    Bug Fix

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/storage/ContentRepository.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/storage/ContentRepository.kt
@@ -4,9 +4,11 @@ import io.vertx.core.json.JsonObject
 import java.io.File
 
 class ContentRepository(private val path: String = "/data/content") {
+
     fun save(widgetId: String, content: JsonObject) {
-        File("$path/$widgetId.json")
-                .writeText(content.toString())
+        if (File(path).exists()) {
+            File("$path/$widgetId.json").writeText(content.toString())
+        }
     }
 
     fun get(widgetId: String): JsonObject {
