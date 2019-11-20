bba9e18fd1b916d73b2c8a1b1e5d923beb3052a4
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2019-11-20 12:48:41 +0100

    Added log when folder for saving widget state does not exist

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/storage/ContentRepository.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/storage/ContentRepository.kt
@@ -1,6 +1,7 @@
 package com.cognifide.cogboard.storage
 
 import io.vertx.core.json.JsonObject
+import io.vertx.core.logging.LoggerFactory
 import java.io.File
 
 class ContentRepository(private val path: String = "/data/content") {
@@ -8,11 +9,17 @@ class ContentRepository(private val path: String = "/data/content") {
     fun save(widgetId: String, content: JsonObject) {
         if (File(path).exists()) {
             File("$path/$widgetId.json").writeText(content.toString())
+        } else {
+            LOGGER.warn("Content for $widgetId not saved because folder $path does not exist")
         }
     }
 
     fun get(widgetId: String): JsonObject {
         val file = File("$path/$widgetId.json")
         return if (file.exists()) JsonObject(file.readText()) else JsonObject()
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(ContentRepository::class.java)
     }
 }
