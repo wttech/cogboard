e58d614a18658d09e0c0762d0d289a0369396586
mateusz.bartkowiak <mateusz.bartkowiak@cognifide.com>
2019-11-25 11:38:26 +0100

    CLAN6-199 | Updated admins validator.

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/config/validation/admins/AdminsValidator.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/config/validation/admins/AdminsValidator.kt
@@ -1,13 +1,39 @@
 package com.cognifide.cogboard.config.validation.admins
 
+import com.cognifide.cogboard.config.model.Admins
 import com.cognifide.cogboard.config.validation.Validator
-import java.io.File
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
 
 object AdminsValidator : Validator {
-    override fun validate(config: String): Boolean {
-        if (File("/data/admins.json").exists()) {
-            return true
+
+    private val LOGGER: Logger = LoggerFactory.getLogger(AdminsValidator::class.java)
+    private val mapper = jacksonObjectMapper().disable(MapperFeature.ALLOW_COERCION_OF_SCALARS)
+
+    override fun validate(config: String): Boolean =
+            try {
+                val adminsConfig = mapper.readValue<Admins>(config)
+                validateAdmins(adminsConfig)
+            } catch (error: JsonMappingException) {
+                LOGGER.error(error.message)
+                false
+            }
+
+    private fun validateAdmins(config: Admins): Boolean {
+        if (config.admins.isEmpty()) {
+            return false
+        } else {
+            config.admins.forEach() { admin ->
+                if (admin.name.isEmpty() || admin.pass.isEmpty()) {
+                    return false
+                }
+                return true
+            }
         }
-        return false
+        return true
     }
 }
