7389c18f1b50c5fba48a0399bf9770e8bc549776
Wojciech Majchrzak <wojciech.majchrzak@cognifide.com>
2019-12-03 14:35:54 +0100

    allign to new docker versioning method

--- scripts/runContainer-prod.sh
+++ scripts/runContainer-prod.sh
@@ -4,7 +4,7 @@ APP_PORT=8092
 WS_PORT=9001
 DEBUG_PORT=18092
 CONFIG_DIR=$(pwd)/mnt
-VERSION=${1:-latest}
+DOCKER_TAG=${1:-$VERSION}
 IMAGE_NAME="cogboard/cogboard-app"
 CONTAINER_NAME="cogboard"
 
@@ -15,4 +15,4 @@ docker rm -f cogboard
 docker image prune -f
 
 # Run
-docker run -d -p ${APP_PORT}:${APP_PORT} -p ${WS_PORT}:${WS_PORT} -p ${DEBUG_PORT}:${DEBUG_PORT} -v ${CONFIG_DIR}:/data --name ${CONTAINER_NAME} ${IMAGE_NAME}:${VERSION}
+docker run -d -p ${APP_PORT}:${APP_PORT} -p ${WS_PORT}:${WS_PORT} -p ${DEBUG_PORT}:${DEBUG_PORT} -v ${CONFIG_DIR}:/data --name ${CONTAINER_NAME} ${IMAGE_NAME}:${DOCKER_TAG}
