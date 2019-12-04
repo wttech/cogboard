6cfbc7633bbcfa51f5aa3ac0c773eda122346c35
pankrocog <57038088+pankroscog@users.noreply.github.com>
2019-12-04 11:09:15 +0100

    FIX not updated after edit

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/widget/type/SonarQubeWidget.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/widget/type/SonarQubeWidget.kt
@@ -30,6 +30,8 @@ class SonarQubeWidget(vertx: Vertx, config: JsonObject) : AsyncWidget(vertx, con
             attachMetrics(content, it)
             content.put(CogboardConstants.PROP_URL, "$publicUrl/dashboard/index/$idNumber")
 
+            content.clearErrorMessageAndErrorCause()
+
             send(JsonObject()
                     .put(CogboardConstants.PROP_STATUS, extractStatus(it))
                     .put(CogboardConstants.PROP_CONTENT, content))
