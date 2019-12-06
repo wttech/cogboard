4d39986c7485b861809ed0630bcc4c239769ff7d
Wojciech Majchrzak <wojciech.majchrzak@cognifide.com>
2019-12-06 16:02:34 +0100

    fixed image tagging

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,5 +7,5 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Tag and push recently builded image
 echo $VERSION
-echo "Tag RELEASE build" && docker tag ${DOCKER_IMAGE}:latest ${DOCKER_IMAGE}:${VERSION}
+echo "Tag RELEASE build" && docker tag ${DOCKER_IMAGE}:${VERSION} ${DOCKER_IMAGE}:latest
 echo "Push RELEASE image" && docker push ${DOCKER_IMAGE}
