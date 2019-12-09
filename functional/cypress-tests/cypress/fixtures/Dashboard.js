e345023d309f9a349bcac3607b28f81c58d62ed8
staszke <stanislaw.rosada@gmail.com>
2019-12-09 13:20:28 +0100

    Fixed old test

--- functional/cypress-tests/cypress/fixtures/Dashboard.js
+++ functional/cypress-tests/cypress/fixtures/Dashboard.js
@@ -6,7 +6,4 @@ export let columnEdgeValues = ["3", "4", "20", "21"];
 
 export let switchIntervalEdgeValues = ["2", "3"];
 
-export let dashboardNames = [
-  dashboardNameGen(),
-  "Too Long Dashboard Name Input"
-];
+export let dashboardNames = [dashboardNameGen(), 'Long Dashboard Name Input. It has exactly 51 chars.'];
