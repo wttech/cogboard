8c14683b41dd2011d082285e65751069772903ff
Hubert Ziętek-Fryc <hubert.zietek-fryc@cognifide.com>
2019-12-10 09:23:25 +0100

    bugfix/CLAN6-216-text-widget-display-issues

--- cogboard-webapp/src/components/widgets/types/TextWidget/styled.js
+++ cogboard-webapp/src/components/widgets/types/TextWidget/styled.js
@@ -26,7 +26,6 @@ export const RotatedStyledPre = styled(StyledPre)`
 export const OverflowingText = component => styled(component)`
   overflow: hidden;
   text-overflow: ellipsis;
-  word-wrap: break-word;
 `;
 
 export const SetWidth = (component, componentWidth) => styled(component)`
