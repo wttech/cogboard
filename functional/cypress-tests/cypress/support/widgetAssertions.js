5593b7805664a4fe3d57fb2c965a254660bae89f
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2019-12-18 15:42:34 +0100

    CR fixes

--- functional/cypress-tests/cypress/support/widgetAssertions.js
+++ functional/cypress-tests/cypress/support/widgetAssertions.js
@@ -19,7 +19,6 @@ export function validateBambooPlan() {
 
 export function validateCheckbox() {
   cy.get('[data-cy="checkbox"]')
-    .click()
     .click();
   cy.get('[data-cy="checkbox"]')
     .parents('[draggable="true"]')
