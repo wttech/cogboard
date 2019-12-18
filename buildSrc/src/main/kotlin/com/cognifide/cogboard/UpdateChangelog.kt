c7ce6f35d7455b5c5a3d88345259b36dc20804ff
mateusz.bartkowiak <mateusz.bartkowiak@cognifide.com>
2019-12-18 10:48:30 +0100

    CLAN6-161 | test travis

--- buildSrc/src/main/kotlin/com/cognifide/cogboard/UpdateChangelog.kt
+++ buildSrc/src/main/kotlin/com/cognifide/cogboard/UpdateChangelog.kt
@@ -3,17 +3,20 @@ package com.cognifide.cogboard
 import com.google.gson.Gson
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.options.Option
 import java.io.File
 import java.net.URL
 import java.time.LocalDate
 
 open class UpdateChangelog : DefaultTask() {
 
-    val branch = "160"
-    private val gson: Gson = Gson()
-    private val changeLog = File("changelog.md")
+    @Option(option="branch", description="branch name")
+    private lateinit var branch: String
 
     lateinit var version: String
+
+    private val gson: Gson = Gson()
+    private val changeLog = File("changelog.md")
 
     @TaskAction
     fun updateChangelog() {
