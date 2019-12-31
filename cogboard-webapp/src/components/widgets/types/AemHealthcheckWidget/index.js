8495ef863c44e3535e7e4db64faae2c22915db4a
Wojciech Młyńczak <wojciech.mlynczak@cognifide.com>
2019-12-31 14:07:13 +0100

    Add functionality to AEM Healthcheck

--- cogboard-webapp/src/components/widgets/types/AemHealthcheckWidget/index.js
+++ cogboard-webapp/src/components/widgets/types/AemHealthcheckWidget/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { object } from 'prop-types';
 
 import { Link } from '@material-ui/core';
-import { Caption } from '../../../styled';
+import { Caption, WidgetButton } from '../../../styled';
 
 import { AEM_HEALTH_CHECKS } from '../../../../constants';
 
-const AemHealthcheckWidget = ({ healthChecks }) => {
+const AemHealthcheckWidget = ({ url, healthChecks }) => {
   return (
     <>
-      {Object.entries(healthChecks).map(([name, data]) => (
-        <Link href={data['url']} target="_blank">
-          <Caption key={name}>
-            {AEM_HEALTH_CHECKS[name]}: {data['status']}
-          </Caption>
-        </Link>
-      ))}
+      <div>
+        {Object.entries(healthChecks).map(([name, data]) => (
+          <Link href={data['url']} target="_blank">
+            <Caption key={name}>
+              {AEM_HEALTH_CHECKS[name]}: {data['status']}
+            </Caption>
+          </Link>
+        ))}
+      </div>
+      <WidgetButton href={url}>Healthchecks</WidgetButton>
     </>
   );
 };
