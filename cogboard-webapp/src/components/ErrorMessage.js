60c190d50f88fb1c6394f7c3413251b81f5d5378
Wojciech Młyńczak <wojciech.mlynczak@cognifide.com>
2019-12-31 15:01:39 +0100

    Compress error message

--- cogboard-webapp/src/components/ErrorMessage.js
+++ cogboard-webapp/src/components/ErrorMessage.js
@@ -4,12 +4,12 @@ import { Typography } from '@material-ui/core';
 
 export const ErrorMessage = ({ errorMessage, errorCause }) => {
   return (
-    <>
+    <div>
       <Typography paragraph>{errorMessage}</Typography>
       <Typography variant="caption" paragraph>
         {errorCause}
       </Typography>
-    </>
+    </div>
   );
 };
 
