6f23d32b232e87b2c190c56c998368c68d67467f
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2020-01-23 09:19:29 +0100

    Readme updated with information about widget class

--- functional/cypress-tests/cypress/support/user.js
+++ functional/cypress-tests/cypress/support/user.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add(
-  "login",
-  (username = Cypress.env("username"), password = Cypress.env("password")) => {
+  'login',
+  (username = Cypress.env('username'), password = Cypress.env('password')) => {
     cy.get('[data-cy="user-login-login-icon"]').click();
     cy.get('[data-cy="user-login-username-input"]')
       .clear()
@@ -11,41 +11,41 @@ Cypress.Commands.add(
     cy.get('[data-cy="user-login-submit-button"]').click();
     cy.contains(
       '[data-cy="notification-snackbar"]',
-      "Logged in as " + Cypress.env("username")
-    ).should("is.visible");
-    cy.get('[data-cy="user-login-login-icon"]').should("not.exist");
+      'Logged in as ' + Cypress.env('username')
+    ).should('is.visible');
+    cy.get('[data-cy="user-login-login-icon"]').should('not.exist');
   }
 );
 
-Cypress.Commands.add("logout", () => {
+Cypress.Commands.add('logout', () => {
   cy.get('[data-cy="user-login-logout-icon"]').click();
   const username =
-    Cypress.env("username")[0].toUpperCase() + Cypress.env("username").slice(1);
+    Cypress.env('username')[0].toUpperCase() + Cypress.env('username').slice(1);
   cy.contains(
     '[data-cy="notification-snackbar"]',
-    username + " logged out"
-  ).should("is.visible");
-  cy.get('[data-cy="user-login-logout-icon"]').should("not.exist");
+    username + ' logged out'
+  ).should('is.visible');
+  cy.get('[data-cy="user-login-logout-icon"]').should('not.exist');
 });
 
-Cypress.Commands.add("getAuthenticationToken", () => {
+Cypress.Commands.add('getAuthenticationToken', () => {
   cy.request({
-    method: "POST",
-    url: "/api/login",
+    method: 'POST',
+    url: '/api/login',
     headers: {
-      "Content-Type": "application/json",
-      Accept: "*/*",
-      "Sec-Fetch-Site": "same-origin",
-      "Sec-Fetch-Mode": "cors"
+      'Content-Type': 'application/json',
+      Accept: '*/*',
+      'Sec-Fetch-Site': 'same-origin',
+      'Sec-Fetch-Mode': 'cors'
     },
-    body: { username: "admin", password: "admin" }
+    body: { username: 'admin', password: 'admin' }
   }).then(result => `${result.body.token}`);
 });
 
-Cypress.Commands.add("loginWithToken", () => {
+Cypress.Commands.add('loginWithToken', () => {
   cy.getAuthenticationToken()
-    .then(token => window.localStorage.setItem("token", `${token}`))
+    .then(token => window.localStorage.setItem('token', `${token}`))
     .then(() => {
-      cy.visit("/");
+      cy.visit('/');
     });
 });
