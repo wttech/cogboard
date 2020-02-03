cc850e04bfdff466eab93bf6480f7597c082852f
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2020-02-03 11:05:45 +0100

    rest instead of parsing html

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/widget/type/CatsWidget.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/widget/type/CatsWidget.kt
@@ -8,13 +8,11 @@ import io.vertx.core.json.JsonObject
 class CatsWidget(vertx: Vertx, config: JsonObject) : AsyncWidget(vertx, config) {
 
     override fun updateState() {
-        httpGet(url = "https://random.cat/view/${(1..1000).random()}")
+        httpGet(url = "http://aws.random.cat/meow")
     }
 
     override fun handleResponse(responseBody: JsonObject) {
-        val imageUrl = responseBody.getString("body")
-                ?.substringBefore("\" alt=\"\" title=\"\" id=\"cat\"")
-                ?.substringAfterLast("src=\"")
+        val imageUrl = responseBody.getString("file")
         val content = JsonObject().put("imageUrl", imageUrl)
 
         send(JsonObject().put(CogboardConstants.PROP_CONTENT, content))
