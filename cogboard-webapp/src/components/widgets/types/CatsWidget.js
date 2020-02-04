17c116840a0b20a6d346fa37f09e0eeffe7d4be0
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2020-02-04 12:05:23 +0100

    Better styling

--- cogboard-webapp/src/components/widgets/types/CatsWidget.js
+++ cogboard-webapp/src/components/widgets/types/CatsWidget.js
@@ -2,16 +2,29 @@ import React from 'react';
 import {string} from 'prop-types';
 import styled from '@emotion/styled/macro';
 
+const StyledDiv = styled.div`
+  height: 100%;
+  position: relative;
+`;
+
 const StyledImg = styled.img`
-  max-height: 495px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  display: block;
+  object-fit: cover;
+  object-position: center;
 `;
 
 const CatsWidget = ({imageUrl}) => {
   return (
-    <StyledImg
-      alt="Cute Kitty"
-      src={imageUrl}
-    />
+    <StyledDiv>
+      <StyledImg
+        alt="Cute Kitty"
+        src={imageUrl}
+      />
+    </StyledDiv>
   );
 };
 
