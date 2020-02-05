28596d7f605fe9c35cb9cd7013cffadd04951e54
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2020-02-05 13:53:07 +0100

    Small fixes

--- cogboard-webapp/src/components/EditEndpoint.js
+++ cogboard-webapp/src/components/EditEndpoint.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { useToggle } from '../hooks';
