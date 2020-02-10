7155202a5eb01f996dcbba3c4319f1df7d9a3c23
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2020-02-10 15:43:05 +0100

    BoardsConfigServiceTest test refactored and improved

--- cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/service/BoardsConfigServiceTest.kt
+++ cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/service/BoardsConfigServiceTest.kt
@@ -1,87 +1,73 @@
 package com.cognifide.cogboard.config.service
 
-import com.cognifide.cogboard.config.ConfigType
-import com.cognifide.cogboard.config.validation.boards.BoardsValidator
 import com.cognifide.cogboard.storage.ContentRepository
-import com.cognifide.cogboard.storage.VolumeStorage
-import com.cognifide.cogboard.storage.VolumeStorageFactory.get
 import io.vertx.core.json.JsonObject
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
 
 internal class BoardsConfigServiceTest {
-    @Test
-    fun `Expect board config saved without content`() {
-        //given
-        val boardPath = BoardsConfigServiceTest::class.java.getResource("/board").path
-        val boardConfig = "$boardPath/server-config.json"
 
-        val contentRepository = ContentRepository(boardPath)
-        val underTest = BoardsConfigService(contentRepository, configFile = boardConfig)
+    private val boardPath = BoardsConfigServiceTest::class.java.getResource("/board").path
+    private val boardConfig = "$boardPath/initial-test-config.json"
+
+    private lateinit var underTest: BoardsConfigService
+
+    @BeforeEach
+    fun initService() {
+        underTest = BoardsConfigService(ContentRepository(boardPath), configFile = boardConfig)
+    }
+
+    @Test
+    fun `Expect board config saved without content data`() {
+        // given
+        val newConfigState = File("$boardPath/new-test-config.json").readText()
 
         //when
-        val uiBoardConfigState = File("$boardPath/ui-board-config.json").readText()
-        underTest.saveBoardsConfig(JsonObject(uiBoardConfigState))
+        underTest.saveBoardsConfig(JsonObject(newConfigState))
 
         //then
         val contentOnServer = JsonObject(File(boardConfig).readText()).getJsonObject("widgets")
                 .getJsonObject("widgetsById")
-                .getJsonObject("widget1")
+                .getJsonObject("newWidget")
                 .getJsonObject("content")
         assertTrue(contentOnServer.isEmpty)
     }
 
     @Test
     fun `Expect config merged from server board config and widget states`() {
-        //given
-        val boardPath = BoardsConfigServiceTest::class.java.getResource("/board").path
-        val boardConfig = "$boardPath/server-board-config.json"
-
-        val contentRepository = ContentRepository(boardPath)
-        val underTest = BoardsConfigService(contentRepository, configFile = boardConfig)
-
         //when
         val config = underTest.loadBoardsConfig()
 
         //then
         assertTrue(config.getJsonObject("widgets")
                 .getJsonObject("widgetsById")
-                .getJsonObject("serverWidget1")
+                .getJsonObject("widgetThatIsNotEdited")
                 .getJsonObject("content").containsKey("serverTime"))
     }
 
     @Test
-    fun `Expect all widgets returned`(){
-        //given
-        val boardPath = BoardsConfigServiceTest::class.java.getResource("/board").path
-        val boardConfig = "$boardPath/server-board-config.json"
-        val contentRepository = ContentRepository(boardPath)
-        val underTest = BoardsConfigService(contentRepository, configFile = boardConfig)
-
+    fun `Expect all widgets returned`() {
         //when
         val allWidget = underTest.getAllWidgets()
 
         //then
-        assertTrue(allWidget.containsKey("serverWidget1"))
-        assertTrue(allWidget.size() == 1)
+        assertTrue(allWidget.containsKey("newWidget")
+                || allWidget.containsKey("widgetThatWillBeReplaced")) // depends on execution order
+        assertTrue(allWidget.containsKey("widgetThatIsNotEdited"))
+        assertTrue(allWidget.size() == 2)
     }
 
     @Test
-    fun `Expect content saved`(){
-        //given
-        val boardPath = BoardsConfigServiceTest::class.java.getResource("/board").path
-        val boardConfig = "$boardPath/server-board-config.json"
-
-        val contentRepository = ContentRepository(boardPath)
-        val underTest = BoardsConfigService(contentRepository, configFile = boardConfig)
-
+    fun `Expect content saved`() {
         //when
         underTest.saveContent("testWidget", JsonObject().put("someKey", "someValue"))
 
         //then
         val content = JsonObject(File("$boardPath/content/testWidget.json").readText())
         assertTrue(content.containsKey("someKey"))
+        assertEquals("someValue", content.getString("someKey"))
     }
 }
