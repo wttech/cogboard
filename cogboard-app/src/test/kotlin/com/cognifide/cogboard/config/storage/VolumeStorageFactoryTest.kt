38dde190c8b5e17f7a6f3c41cb6f1a7e8d02c026
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2020-02-11 09:46:17 +0100

    BoardsConfigServiceTest test refactored and improved

--- cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/storage/VolumeStorageFactoryTest.kt
+++ cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/storage/VolumeStorageFactoryTest.kt
@@ -4,21 +4,32 @@ import com.cognifide.cogboard.config.ConfigType
 import com.cognifide.cogboard.storage.VolumeStorageFactory
 import io.vertx.core.json.JsonObject
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
 
 internal class VolumeStorageFactoryTest {
 
     private val boardPath = VolumeStorageFactoryTest::class.java.getResource("/board").path
-    private val configNotExist = "$boardPath/initial-config-for-VolumeStorageFactoryTest-test.json"
+    private val configThatShouldNotExist = "$boardPath/initial-config-for-VolumeStorageFactoryTest-test.json"
+
+    @BeforeEach
+    fun cleanup() {
+        val config = File(configThatShouldNotExist)
+        if (config.exists()) config.delete()
+    }
 
     @Test
     fun `Expect config is created when not present`() {
+        //before
+        assertFalse(File(configThatShouldNotExist).exists())
+
         //when
-        val underTest = VolumeStorageFactory.get(ConfigType.ADMINS, configNotExist)
+        val underTest = VolumeStorageFactory.get(ConfigType.ADMINS, configThatShouldNotExist)
 
         //then
-        val content = JsonObject(File(configNotExist).readText())
+        val content = JsonObject(File(configThatShouldNotExist).readText())
         assertTrue(content.containsKey("admins"))
     }
 }
