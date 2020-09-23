7ba54345b5235e0cfd2094612f5ff83411815452
yahor.mikheyenka <yahor.mikheyenka@cognifide.com>
2020-09-23 10:08:33 +0200

    feature/To-Do-List-Widget - clear all items

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/widget/type/ToDoListWidget.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/widget/type/ToDoListWidget.kt
@@ -17,32 +17,42 @@ class ToDoListWidget(vertx: Vertx, config: JsonObject) : BaseWidget(vertx, confi
     }
 
     override fun updateState() {
-        val selectedItems = selectedItems()
-        send(contentJson(selectedItems))
+        val selectedItems = loadSelectedItems()
+        send(wrapBeforeSending(selectedItems))
     }
 
     private fun changeSelectedItems(state: JsonObject): JsonObject {
-        val selectedItems = selectedItems()
+        val selectedItems: JsonArray
+        if (state.containsKey(CLEAR_ITEMS) && state.getBoolean(CLEAR_ITEMS)) {
+            selectedItems = JsonArray()
+        } else {
+            selectedItems = loadSelectedItems()
+            addOrRemoveItem(state, selectedItems)
+        }
+        return wrapBeforeSending(selectedItems)
+    }
+
+    private fun addOrRemoveItem(state: JsonObject, selectedItems: JsonArray) {
         val selectedItem = state.getValue(SELECTED_ITEM)
         if (selectedItems.contains(selectedItem)) {
             selectedItems.remove(selectedItem)
         } else {
             selectedItems.add(selectedItem)
         }
-        return contentJson(selectedItems)
     }
 
-    private fun selectedItems(): JsonArray {
+    private fun loadSelectedItems(): JsonArray {
         val widget = ContentRepository().get(id)
         return widget.getJsonArray(SELECTED_ITEMS) ?: JsonArray()
     }
 
-    private fun contentJson(selectedItems: JsonArray? = JsonArray()) =
+    private fun wrapBeforeSending(selectedItems: JsonArray? = JsonArray()) =
             JsonObject().put(CogboardConstants.PROP_CONTENT, JsonObject().put(SELECTED_ITEMS, selectedItems))
 
     companion object {
         val PROPS = setOf("toDoListItems")
         const val SELECTED_ITEM = "selectedItem"
         const val SELECTED_ITEMS = "selectedItems"
+        const val CLEAR_ITEMS = "clearItems"
     }
 }
