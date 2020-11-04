e58f6905530139693d72d720b82903104a8e055e
Szymon Owczarzak <szymon.owczarzak@gmail.com>
2020-11-04 15:29:00 +0100

    Update BoardsConfigServiceTest.kt

--- cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/service/BoardsConfigServiceTest.kt
+++ cogboard-app/src/test/kotlin/com/cognifide/cogboard/config/service/BoardsConfigServiceTest.kt
@@ -34,7 +34,7 @@ internal class BoardsConfigServiceTest {
         assertTrue(contentOnServer.isEmpty)
     }
 
-    //@Test
+    @Test
     fun `Expect widget content merged to config`() {
         // given
         val underTest = initService()
