7d495aa595391f5bd5774868705223d6aed5653c
Grzegorz Bednarski <grzegorz.bednarski@cognifide.com>
2021-04-07 19:12:54 +0200

    cypress configs

--- functional/cypress-tests/cypress/plugins/index.js
+++ functional/cypress-tests/cypress/plugins/index.js
@@ -12,11 +12,20 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
+// promisified fs module
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile(file) {
+    const pathToConfigFile = path.resolve(`${file}.env.json`)
+
+    return fs.readJson(pathToConfigFile)
+}
+
+// plugins file
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-};
+    // accept a configFile value or use development by default
+    const file = config.env.configFile || 'local'
+
+    return getConfigurationByFile(file)
+}
