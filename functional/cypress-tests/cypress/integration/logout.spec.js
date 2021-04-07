d8b6edd978477a7ad5c07f9e87a99acad670b8d3
Grzegorz Bednarski <grzegorz.bednarski@cognifide.com>
2021-04-07 22:49:34 +0200

    logout.spec

---
+++ functional/cypress-tests/cypress/integration/logout.spec.js
@@ -0,0 +1,40 @@
+import {MAIN_SCREEN} from '../fixtures/selectors.js';
+
+describe('Logout', () => {
+    context('Admin logout ', () => {
+        before(() => {
+            cy.clearStorage();
+            cy.visit('/');
+        });
+        it('Login', () => {
+            cy.login();
+        });
+        it('Logout', () => {
+            cy.logout();
+        });
+        it('See login button', () => {
+            cy.isVisible(MAIN_SCREEN.LOGIN_BUTTON);
+        });
+        it('No logout button', () => {
+            cy.notExist(MAIN_SCREEN.LOGOUT_BUTTON);
+        });
+    });
+    context('Guest logout ', () => {
+        before(() => {
+            cy.clearStorage();
+            cy.visit('/');
+        });
+        it('Login', () => {
+            cy.guestLogin();
+        });
+        it('Logout', () => {
+            cy.logout();
+        });
+        it('See login button', () => {
+            cy.isVisible(MAIN_SCREEN.LOGIN_BUTTON);
+        });
+        it('No logout button', () => {
+            cy.notExist(MAIN_SCREEN.LOGOUT_BUTTON);
+        });
+    });
+});
