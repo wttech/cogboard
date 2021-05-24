90303eb7f41616f964bc1b5da3df8a5bd2fca662
COGNET\krzysztof.piasecki <krzysztof.piasecki@wundermanthompson.com>
2021-05-24 16:48:18 +0200

    Cloak doesnt fit widget

--- cogboard-webapp/src/components/widgets/types/WorldClockWidget/styled.js
+++ cogboard-webapp/src/components/widgets/types/WorldClockWidget/styled.js
@@ -5,9 +5,11 @@ export const TimePre = styled.pre`
   font-family: inherit;
   font-size: 180%;
   margin: 0 auto;
+  line-height: 0.5;
 `;
 
 export const DatePre = styled.pre`
   font-family: inherit;
   margin: 0.25em 0 auto;
+  line-height: 1;
 `;
