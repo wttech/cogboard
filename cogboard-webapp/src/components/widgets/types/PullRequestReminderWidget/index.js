78867d8fda3282a61eed7eba99ef01fa555fe713
michal.gasiorek <michal.gasiorek@wundermanthompson.com>
2021-06-24 14:11:35 +0200

    Remove unnecessary test objects

--- cogboard-webapp/src/components/widgets/types/PullRequestReminderWidget/index.js
+++ cogboard-webapp/src/components/widgets/types/PullRequestReminderWidget/index.js
@@ -6,33 +6,14 @@ import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import { StyledNoItemsInfo } from '../../../Widget/styled';
 import { PullRequestContainer, PullRequestLink } from './styled';
 
-const testPRs = [
-  {
-    id: 0,
-    title: 'Test title',
-    html_url: 'google.pl'
-  },
-  {
-    id: 1,
-    title: 'Test title',
-    html_url: 'google.pl'
-  },
-  {
-    id: 2,
-    title: 'Test title',
-    html_url: 'google.pl'
-  }
-];
-
 const PullRequestReminderWidget = ({ pullRequests }) => {
   if (!pullRequests) {
-    pullRequests = testPRs;
-    // return (
-    //   <StyledNoItemsInfo>
-    //     <InfoOutlinedIcon fontSize="large" />
-    //       <p>API rate limit exceeded!</p>
-    //   </StyledNoItemsInfo>
-    // )
+    return (
+      <StyledNoItemsInfo>
+        <InfoOutlinedIcon fontSize="large" />
+        <p>API rate limit exceeded!</p>
+      </StyledNoItemsInfo>
+    );
   }
 
   return (
