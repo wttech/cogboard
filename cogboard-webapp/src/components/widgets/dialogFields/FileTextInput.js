7178432254c4d11022571af67f86875f6e049bb5
Przemysław Ambroży <przemek19986@gmail.com>
2021-11-02 20:38:03 +0100

    Change text fo file

---
+++ cogboard-webapp/src/components/widgets/dialogFields/FileTextInput.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+import { Button, InputLabel } from '@material-ui/core';
+import { StyledValidationMessages } from '../../WidgetForm/styled';
+import {
+  DeleteButton,
+  StyledHorizontalStack,
+  StyledLabel,
+  StyledVerticalStack
+} from './styled';
+
+const FileTextInput = ({
+  error,
+  dataCy,
+  values,
+  label,
+  value,
+  onChange,
+  ...other
+}) => {
+  const [filename, setFilename] = useState('');
+
+  const getFileContents = async e => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = async e => {
+      const text = e.target.result;
+      let event = e;
+      event.target.value = text;
+      onChange(event);
+      alert(text);
+      console.log(text);
+    };
+    reader.readAsText(file);
+    setFilename(file.name);
+  };
+
+  const deleteFile = e => {
+    e.preventDefault();
+    setFilename('');
+    let event = e;
+    event.target.value = '';
+    onChange(event);
+  };
+
+  const fileInfo =
+    filename === '' ? null : (
+      <StyledHorizontalStack>
+        <p>{filename}</p>
+        <DeleteButton variant="contained" onClick={e => deleteFile(e)}>
+          Delete
+        </DeleteButton>
+      </StyledHorizontalStack>
+    );
+
+  return (
+    <StyledVerticalStack>
+      <StyledLabel>SSH Key</StyledLabel>
+      <StyledHorizontalStack>
+        <Button variant="contained" component="label">
+          Upload a File
+          <input type="file" hidden onChange={e => getFileContents(e)} />
+        </Button>
+        {fileInfo}
+      </StyledHorizontalStack>
+      <StyledValidationMessages messages={error} data-cy={`${dataCy}-error`} />
+    </StyledVerticalStack>
+  );
+};
+
+export default FileTextInput;
