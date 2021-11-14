d6a616f7bfdb15f0fcb284095183457c176d0b65
Michał Przypaśniak <m.przypasniak99@gmail.com>
2021-11-14 17:26:12 +0100

    Fixes after WTTech review #414

--- cogboard-app/src/main/kotlin/com/cognifide/cogboard/ssh/session/strategy/SSHKeyAuthSessionStrategy.kt
+++ cogboard-app/src/main/kotlin/com/cognifide/cogboard/ssh/session/strategy/SSHKeyAuthSessionStrategy.kt
@@ -3,10 +3,11 @@ package com.cognifide.cogboard.ssh.session.strategy
 import com.cognifide.cogboard.ssh.auth.SSHAuthData
 import com.jcraft.jsch.JSch
 import com.jcraft.jsch.Session
+import io.netty.util.internal.StringUtil.EMPTY_STRING
 
 class SSHKeyAuthSessionStrategy(jSch: JSch, authData: SSHAuthData) : SessionStrategy(jSch, authData) {
     override fun initSession(): Session {
-        if (authData.password == "") {
+        if (authData.password == EMPTY_STRING) {
             jsch.addIdentity(securityString)
         } else {
             jsch.addIdentity(securityString, authData.password)
