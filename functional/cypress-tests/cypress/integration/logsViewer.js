56c18c9127264d053f7b20ba84505a453a34ef9b
clmrv <everay@wp.pl>
2021-12-19 22:58:17 +0100

    Add filters multiselect test #440

--- functional/cypress-tests/cypress/integration/logsViewer.js
+++ functional/cypress-tests/cypress/integration/logsViewer.js
@@ -19,8 +19,17 @@ describe('Logs Viewer', () => {
   });
 
   describe('Filters', () => {
-    const label = 'starts with a';
-    const regExp = '^a';
+    const logsMatchFilter = regExp => {
+      cy.get('[data-cy="log-entry"] ').each(log => {
+        cy.wrap(log).contains(
+          '[data-cy="log-variable-data"] p',
+          new RegExp(regExp)
+        );
+      });
+    };
+
+    const first = { label: 'starts with a', regExp: '^a' };
+    const second = { label: 'ends with a', regExp: 'a$' };
 
     it('Advanced filters modal shows', () => {
       widget.click('[data-cy="advanced-filters-button"]');
@@ -33,11 +42,11 @@ describe('Logs Viewer', () => {
       widget.assertText('h2', 'Add new filter');
       cy.get('[data-cy="filter-form-label-input"]')
         .clear()
-        .type(label);
-      cy.get('[data-cy="filter-form-reg-exp-input"]').type(regExp);
+        .type(first.label);
+      cy.get('[data-cy="filter-form-reg-exp-input"]').type(first.regExp);
       widget.click('[data-cy="filter-form-submit-button"]');
 
-      widget.assertText('span.MuiListItemText-primary', label);
+      widget.assertText('span.MuiListItemText-primary', first.label);
       widget.isChecked(
         '.MuiListItemSecondaryAction-root input[type="checkbox"]',
         true
@@ -46,19 +55,26 @@ describe('Logs Viewer', () => {
       widget.click('[data-cy="advanced-filters-menu-exit-button"]');
     });
 
-    it('Filter shows in multiselect', () => {
-      widget.assertText('.MuiChip-label', label);
+    it('Filter removes logs correctly', () => {
+      logsMatchFilter(first.regExp);
     });
 
-    it('Filter filters logs', () => {
-      cy.get('[data-cy="log-entry"] ').each(log => {
-        cy.wrap(log).contains(
-          '[data-cy="log-variable-data"] p',
-          new RegExp(regExp)
-        );
-      });
+    it('Filter multiselect dialog works', () => {
+      widget.assertText('[data-cy="filters-chip"] .MuiChip-label', first.label);
+      widget.click('[data-cy="filters-chip"] .MuiChip-deleteIcon');
+      cy.contains(
+        '[data-cy="filters-chip"] .MuiChip-label',
+        first.label
+      ).should('not.exist');
+
+      widget.click('[data-cy="filters-menu"]');
+      widget.click('[data-cy="filters-menu-option"]');
+      cy.get('[data-cy="filters-menu-option"]').type('{esc}');
+      widget.assertText('[data-cy="filters-chip"] .MuiChip-label', first.label);
     });
   });
+
+  // data-cy="filters-chip"
 
   // doesn't work
 
