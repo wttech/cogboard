8f7040f6cae1eed409846dd16a9683444a9f3444
clmrv <everay@wp.pl>
2021-12-22 19:35:05 +0100

    Rework unit tests #440

--- cogboard-webapp/src/components/widgets/types/LogViewerWidget/LogList/filterByRegExp.test.js
+++ cogboard-webapp/src/components/widgets/types/LogViewerWidget/LogList/filterByRegExp.test.js
@@ -5,73 +5,85 @@ const logHeader = {
   seq: 228,
   insertedOn: 1639237459,
   date: '2021-12-11T15:44:00',
-  type: 'DEBUG',
-  variableData: null
+  type: 'DEBUG'
 };
 
-it('filter by regexp', () => {
-  const filters = [
+const log = {
+  ...logHeader,
+  variableData: [
     {
-      id: 'filter-3ea57f8b-6c1c-4e2f-bbc5-760aa0526e8e',
-      checked: true,
-      regExp: '^a',
-      label: 'starts with a'
+      header: 'FelixStartLevel',
+      description: 'No description'
     },
     {
-      id: 'filter-2afee54b-8ab6-4a0c-81db-9c92e7bf5203',
-      checked: true,
-      label: 'velit',
-      regExp: 'velit'
-    },
-    {
-      id: 'filter-b9b72673-e6ca-481d-996a-293527032764',
-      checked: true,
-      label: 'starts with F',
-      regExp: '^F'
-    },
-    {
-      id: 'filter-c9dac795-43c6-4a57-9329-5dd1573c9bf4',
-      checked: true,
-      label: 'ends with suscipit',
-      regExp: 'suscipit$'
+      header: 'id velit. vel pretium. ipsum suscipit',
+      description: 'No message description'
     }
-  ];
+  ]
+};
+const emptyLog = {
+  ...logHeader,
+  variableData: [
+    {
+      header: '',
+      description: ''
+    }
+  ]
+};
 
-  const log = {
-    ...logHeader,
-    variableData: [
-      {
-        header: 'FelixStartLevel',
-        description: 'No description'
-      },
-      {
-        header: 'id velit. vel pretium. ipsum suscipit',
-        description: 'No message description'
-      }
-    ]
-  };
-  const emptyLog = {
-    ...logHeader,
-    variableData: [
-      {
-        header: '',
-        description: ''
-      }
-    ]
-  };
+const filters = {
+  startsWithA: {
+    id: 'filter-3ea57f8b-6c1c-4e2f-bbc5-760aa0526e8e',
+    checked: true,
+    regExp: '^a',
+    label: 'starts with a'
+  },
+  velit: {
+    id: 'filter-2afee54b-8ab6-4a0c-81db-9c92e7bf5203',
+    checked: true,
+    label: 'velit',
+    regExp: 'velit'
+  },
+  startsWithF: {
+    id: 'filter-b9b72673-e6ca-481d-996a-293527032764',
+    checked: true,
+    label: 'starts with F',
+    regExp: '^F'
+  },
+  endWithSuscipit: {
+    id: 'filter-c9dac795-43c6-4a57-9329-5dd1573c9bf4',
+    checked: true,
+    label: 'ends with suscipit',
+    regExp: 'suscipit$'
+  }
+};
 
-  expect(filterByRegExp(log, [])).toEqual(true);
+it('shows log without filters', () =>
+  expect(filterByRegExp(log, [])).toBeTruthy());
 
-  // starts with a; velit
-  expect(filterByRegExp(log, [filters[0], filters[1]])).toEqual(false);
-  // starts with a(OFF); velit
+it('filters with multiple rules', () =>
   expect(
-    filterByRegExp(log, [{ ...filters[0], checked: false }, filters[1]])
-  ).toEqual(true);
-  // velit; starts with F; ends with suscipit
-  expect(filterByRegExp(log, [filters[1], filters[2], filters[3]])).toEqual(
-    true
-  );
+    filterByRegExp(log, [filters.startsWithA, filters.velit])
+  ).toBeFalsy());
 
-  expect(filterByRegExp(emptyLog, filters)).toEqual(false);
-});
+it('filters with multiple rules 2', () =>
+  expect(
+    filterByRegExp(log, [
+      filters.velit,
+      filters.startsWithF,
+      filters.endWithSuscipit
+    ])
+  ).toBeTruthy());
+
+it('checks if filter is enabled', () =>
+  expect(
+    filterByRegExp(log, [
+      { ...filters.startsWithA, checked: false },
+      filters.velit
+    ])
+  ).toBeTruthy());
+
+it('filters empty logs', () =>
+  expect(
+    filterByRegExp(emptyLog, [filters.startsWithA, filters.endWithSuscipit])
+  ).toBeFalsy());
