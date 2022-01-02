04384ae0bd5ef682b9a8b475de433bf86ae6bc52
clmrv <everay@wp.pl>
2022-01-02 23:06:23 +0100

    Move scrollbar of toolbar #447

--- cogboard-webapp/src/components/widgets/types/LogViewerWidget/Toolbar/styled.js
+++ cogboard-webapp/src/components/widgets/types/LogViewerWidget/Toolbar/styled.js
@@ -11,14 +11,7 @@ export const Wrapper = styled.div`
   flex-direction: row;
   justify-content: flex-start;
   gap: 1em;
-
-
-  /* hide scrollbar */
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  -ms-overflow-style: none;  
-  scrollbar-width: none; 
+  overflow-x: hidden;
 }
 `;
 
