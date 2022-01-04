45d9af2bd4284a54e278132e5b5d1e6c702418d9
clmrv <everay@wp.pl>
2022-01-04 20:39:48 +0100

    Remove time consts, update test name #441

--- cogboard-webapp/src/components/widgets/types/LogViewerWidget/LogList/filterByDateSpan.test.js
+++ cogboard-webapp/src/components/widgets/types/LogViewerWidget/LogList/filterByDateSpan.test.js
@@ -9,30 +9,28 @@ const log = {
   variableData: []
 };
 
-const spanTime10 = moment('2021-12-22T10:00:00.000Z');
-const logTime15 = '2021-12-22T15:00:00.000';
-const spanTime19 = moment('2021-12-22T19:00:00.000Z');
-const spanTime20 = moment('2021-12-22T20:00:00.000Z');
-
 it('filters out log without date', () =>
   expect(
-    filterByDateSpan(log, { begin: spanTime10, end: spanTime20 })
+    filterByDateSpan(log, {
+      begin: moment('2021-12-22T10:00:00.000Z'),
+      end: moment('2021-12-22T20:00:00.000Z')
+    })
   ).toBeFalsy());
 
-it('should be truthy when no date span is provided', () =>
+it('should filter in when no date span is provided', () =>
   expect(filterByDateSpan(log, { begin: null, end: null })).toBeTruthy());
 
 it('filters by begin date', () => {
   expect(
     filterByDateSpan(
-      { ...log, date: logTime15 },
-      { begin: spanTime10, end: null }
+      { ...log, date: '2021-12-22T15:00:00.000' },
+      { begin: moment('2021-12-22T10:00:00.000Z'), end: null }
     )
   ).toBeTruthy();
   expect(
     filterByDateSpan(
       { ...log, date: '2021-12-22T20:00:00' },
-      { begin: spanTime20, end: null }
+      { begin: moment('2021-12-22T20:00:00.000Z'), end: null }
     )
   ).toBeFalsy();
 });
@@ -40,14 +38,14 @@ it('filters by begin date', () => {
 it('filters by end date', () => {
   expect(
     filterByDateSpan(
-      { ...log, date: logTime15 },
-      { begin: null, end: spanTime20 }
+      { ...log, date: '2021-12-22T15:00:00.000' },
+      { begin: null, end: moment('2021-12-22T20:00:00.000Z') }
     )
   ).toBeTruthy();
   expect(
     filterByDateSpan(
-      { ...log, date: logTime15 },
-      { begin: null, end: spanTime10 }
+      { ...log, date: '2021-12-22T15:00:00.000' },
+      { begin: null, end: moment('2021-12-22T10:00:00.000Z') }
     )
   ).toBeFalsy();
 });
@@ -55,14 +53,20 @@ it('filters by end date', () => {
 it('filters by date span', () => {
   expect(
     filterByDateSpan(
-      { ...log, date: logTime15 },
-      { begin: spanTime10, end: spanTime20 }
+      { ...log, date: '2021-12-22T15:00:00.000' },
+      {
+        begin: moment('2021-12-22T10:00:00.000Z'),
+        end: moment('2021-12-22T20:00:00.000Z')
+      }
     )
   ).toBeTruthy();
   expect(
     filterByDateSpan(
-      { ...log, date: logTime15 },
-      { begin: spanTime19, end: spanTime20 }
+      { ...log, date: '2021-12-22T15:00:00.000' },
+      {
+        begin: moment('2021-12-22T19:00:00.000Z'),
+        end: moment('2021-12-22T20:00:00.000Z')
+      }
     )
   ).toBeFalsy();
 });
