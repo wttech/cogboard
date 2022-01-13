64cb74d09fff2208b77db28df8b6dc2307a7e2b5
Przemysław Ambroży <przemek19986@gmail.com>
2022-01-14 00:16:28 +0100

    Add chmod script, add a copy using slice

--- cogboard-webapp/src/components/widgets/types/LogViewerWidget/helpers.js
+++ cogboard-webapp/src/components/widgets/types/LogViewerWidget/helpers.js
@@ -1,5 +1,5 @@
 export const joinLogs = (currentLogs, newLogs, logLines) => {
-  let joined = currentLogs;
+  let joined = currentLogs.slice();
   newLogs.forEach(newLog => {
     if (!joined.some(log => log._id === newLog._id)) {
       joined.push(newLog);
